499388905b7fb5fa608c92c386d7638c0a780fc3
harmyko <ugniusteiserskis2005@gmail.com>
2025-06-14 16:52:38 +0300

    Initialized the project.

---
+++ src/main/java/com/eventsync/EventsyncApplication.java
@@ -0,0 +1,12 @@
+package com.eventsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsyncApplication.class, args);
+	}
+}
